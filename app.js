73fcde1f7b0dc796f6262fbf2a62b2218b4f3d8f
Craig <craig@oppkey>
2020-01-18 14:14:38 -0800

    finished video 326

--- app.js
+++ app.js
@@ -6,48 +6,80 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const articleSchema = {
-    title: String,
-    content: String
+  title: String,
+  content: String
 };
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res){
-    Article.find(function(err, foundArticles){
-        if(!err){
-            res.send(foundArticles);
-        } else {
-            res.send(err);
-        }
-        
+//////Requests Targetting All Articles///////
+app
+  .route("/articles")
+
+  .get(function(req, res) {
+    Article.find(function(err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
     });
-});
+  })
 
-app.post("/articles", function(req, res){
-
+  .post(function(req, res) {
     const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+      title: req.body.title,
+      content: req.body.content
     });
-    newArticle.save(function(err){
-        if(!err){
-            res.send("Successfully added new article");
-        } else{
-            res.send(err);
+    newArticle.save(function(err) {
+      if (!err) {
+        res.send("Successfully added new article");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .delete(function(req, res) {
+    Article.deleteMany(function(err) {
+      if (!err) {
+        res.send("Successfully deleted all articles");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+//////Requests Targetting A Specific Articles///////
+
+app.route("/articles/:articleTitle")
+
+  .get(function(req, res){
+    console.log(req.params.articleTitle);
+    Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+        if(foundArticle){
+            res.send(foundArticle);
+        } else {
+            res.send("No articles matching that title was found.");
         }
     });
-});
+  });
 
-app.listen(3000, function(){
-    console.log("Server started on port 3000");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
